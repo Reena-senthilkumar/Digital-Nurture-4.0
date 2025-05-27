ae3a7196a641e9a7aaaa6becd58d8aab41430702
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-05-27 07:59:24 +0530

    Create Exercise 2.java

---
+++ JAVA/Exercise 2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        char operator = sc.next().charAt(0);
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero is not allowed.");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+
+        sc.close();
+    }
+}
